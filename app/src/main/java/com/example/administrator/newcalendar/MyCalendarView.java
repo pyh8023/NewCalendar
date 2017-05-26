e2934b00733fc960b76d8d8618ff26cb78c27afe
PYH8023 <1239026329@qq.com>
2017-05-26 22:29:04 +0800

    init

---
+++ app/src/main/java/com/example/administrator/newcalendar/MyCalendarView.java
@@ -0,0 +1,24 @@
+package com.example.administrator.newcalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2017/5/26.
+ */
+
+public class MyCalendarView extends LinearLayout {
+    public MyCalendarView(Context context) {
+        super(context);
+    }
+
+    public MyCalendarView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyCalendarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
